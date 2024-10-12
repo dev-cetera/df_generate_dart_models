af8b0ab56f2ba203a71ce98076f218104bc07ccb
robmllze <86869208+robmllze@users.noreply.github.com>
2024-10-12 20:42:26 +1100

    +chore: Add basic model generator binary

---
+++ bin/genmdl_dart_basic.dart
@@ -0,0 +1,23 @@
+//.title
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// Dart/Flutter (DF) Packages by DevCetra.com & contributors. The use of this
+// source code is governed by an MIT-style license described in the LICENSE
+// file located in this project's root directory.
+//
+// See: https://opensource.org/license/mit
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//.title~
+
+import 'package:df_generate_dart_models/df_generate_dart_models.dart';
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+void main(List<String> args) async {
+  await runGenerateDartModelsApp([
+    ...args,
+    '--template',
+    'https://raw.githubusercontent.com/robmllze/df_generate_dart_models_core/main/templates/basic_v1.dart.md',
+  ]);
+}
